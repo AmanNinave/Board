f2329cd3f32f978fbe0e1d2f098228a08b900749
Aman Ninave <amanninave@Amans-MacBook-Air.local>
2024-11-01 22:12:23 +0530

    favorites and search functionality completed

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -9,6 +9,9 @@ import { Footer } from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Actions } from "@/components/ui/actions";
 import { MoreHorizontal } from "lucide-react";
+import { useApiMutation } from "@/components/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 interface BoardCardProps {
     id : string;
     title : string;
@@ -35,6 +38,19 @@ export const BoardCard = ({
     const createdAtLabel = formatDistanceToNow(createdAt , {
         addSuffix : true
     })
+
+    const {mutate: onFavorite , pending: pendingFavorite} = useApiMutation(api.board.favorite);
+    const {mutate: onUnfavorite , pending: pendingUnfavorite} = useApiMutation(api.board.unfavorite);
+
+    const toggleFavorite = () => {
+        if(isFavorite){
+            onUnfavorite({id})
+                .catch((e) => { toast.error("Failed to unfavorite")});
+        }else{
+            onFavorite({id , orgId})
+                .catch((e) => {toast.error("Failed to favorite")});
+        }
+    }
 
     return (
         <Link href={`/board/${id}`}>
@@ -65,8 +81,8 @@ export const BoardCard = ({
                     title={title}
                     authorLabel={authorLabel}
                     createdAtLabel={createdAtLabel}
-                    onClick={() =>{}}
-                    disabled={false}
+                    onClick={toggleFavorite}
+                    disabled={pendingFavorite || pendingUnfavorite}
                 />
             </div>
         </Link>
